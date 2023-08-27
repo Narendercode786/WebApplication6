f2085f57abc910f5b1f6f16a5926d5cca93b9c6f
Narender <kumar786narender@gmail.com>
2023-08-27 22:04:05 +0530

    Add project files.

---
+++ WebApplication6/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication6.Models;
+using WebApplication6.Services;
+
+namespace WebApplication6.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public List<Employee> employees;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            EmployeeService employeeService = new EmployeeService();
+            employees = employeeService.GetEmployees();
+        }
+    }
+}
---
+++ WebApplication6/Services/EmployeeService.cs
@@ -0,0 +1,47 @@
+﻿using System.Data.SqlClient;
+using System.Security.Cryptography.Xml;
+using WebApplication6.Models;
+
+namespace WebApplication6.Services
+{
+    public class EmployeeService
+    {
+        private static string db_source = "appdb786.database.windows.net";
+        private static string db_user = "admindb";
+        private static string db_password = "test@123";
+        private static string db_database = "appdb";
+
+
+        private SqlConnection GetConnectionString()
+        {
+            var _builder = new SqlConnectionStringBuilder();
+            _builder.DataSource = db_source;
+            _builder.UserID = db_user;
+            _builder.Password = db_password;
+            _builder.InitialCatalog = db_database;
+            return new SqlConnection(_builder.ConnectionString);
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            SqlConnection sqlConnection = GetConnectionString();
+            List<Employee> employees = new List<Employee>();
+            string str = "select Id,Name,Address from Employee With(nolock)";
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand(str, sqlConnection);
+            using(SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Employee emp = new Employee();
+                    emp.Id = Convert.ToInt32(reader["Id"]);
+                    emp.Name = reader["Name"].ToString();
+                    emp.Address = reader["Address"].ToString();
+                    employees.Add(emp);
+                }
+            }
+            sqlConnection.Close();
+            return employees; 
+        }
+    }
+}
